0f23b53bede3029f864d8b89c2e680c805176c93
GazerSuthy <71653550+GazerSuthy@users.noreply.github.com>
2022-03-18 08:52:43 -0400

    Delete day_one directory

--- day_one/view/form.php
+++
@@ -1,6 +0,0 @@
-<main class="enter-input">
-    <form action="" method="post" >
-        <label for="num-input" class="user-input-label">Please Enter A Number:</label>
-        <input type="text" name="user-input" id="user-input">
-    </form>
-</main>
